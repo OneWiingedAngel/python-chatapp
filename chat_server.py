cb3963bfe6063b6593b6d0732c06b9fbfd9d222f
katch <79490292+ka-chng@users.noreply.github.com>
2024-03-25 19:53:25 +0300

    Update chat_server.py

--- chat_server.py
+++ chat_server.py
@@ -2,46 +2,39 @@ import os
 import socket
 import threading
 
-# Get the port from the environment variable PORT or default to 443
 PORT = int(os.environ.get('PORT', 443))
-HOST = '0.0.0.0'  # Listen on all network interfaces
+HOST = '0.0.0.0'
 
-# Function to handle client connections
 def handle_client(client, username):
     print(f"[NEW CONNECTION] {username} connected.")
 
     connected = True
     while connected:
-        # Receive message from client
         msg = client.recv(1024).decode('utf-8')
         if msg == "quit":
             connected = False
         else:
-            print(f"[{username}] {msg}")
             broadcast(username, msg)
 
     client.close()
 
-# Function to broadcast message to all clients except the sender
-def broadcast(username, msg):
-    for client, _ in clients:
+def broadcast(sender_username, msg):
+    for client_socket, username in clients:
         try:
-            client.send(f"[{username}] {msg}".encode('utf-8'))
+            if username != sender_username:
+                client_socket.send(f"[{sender_username}] {msg}".encode('utf-8'))
         except BrokenPipeError:
-            # Handle the broken pipe error (client disconnected)
-            print(f"Client disconnected unexpectedly.")
+            print(f"Client {username} disconnected unexpectedly.")
+            clients.remove((client_socket, username))
 
-# List to store connected clients
 clients = []
 
-# Initialize server socket
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((HOST, PORT))
 server.listen()
 
 print(f"[LISTENING] Server is listening on port {PORT}")
 
-# Accept incoming connections and start a new thread for each client
 while True:
     client_socket, _ = server.accept()
     username = client_socket.recv(1024).decode('utf-8')
